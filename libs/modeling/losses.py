7fad96c1c8f22dce2f0c021c1c032c88aacffa87
happyharrycn <happyharrycn@gmail.com>
2022-05-07 01:28:27 -0500

    Added DIoU loss; Simplified the logic for GIoU/IoU loss.

--- libs/modeling/losses.py
+++ libs/modeling/losses.py
@@ -95,15 +95,70 @@ def ctr_giou_loss_1d(
     unionk = (lp + rp) + (lg + rg) - intsctk
     iouk = intsctk / unionk.clamp(min=eps)
 
+    # giou is reduced to iou in our setting, skip unnecessary steps
+    loss = 1.0 - iouk
+
+    if reduction == "mean":
+        loss = loss.mean() if loss.numel() > 0 else 0.0 * loss.sum()
+    elif reduction == "sum":
+        loss = loss.sum()
+
+    return loss
+
+@torch.jit.script
+def ctr_diou_loss_1d(
+    input_offsets: torch.Tensor,
+    target_offsets: torch.Tensor,
+    reduction: str = 'none',
+    eps: float = 1e-8,
+) -> torch.Tensor:
+    """
+    Distance-IoU Loss (Zheng et. al)
+    https://arxiv.org/abs/1911.08287
+
+    This is an implementation that assumes a 1D event is represented using
+    the same center point with different offsets, e.g.,
+    (t1, t2) = (c - o_1, c + o_2) with o_i >= 0
+
+    Reference code from
+    https://github.com/facebookresearch/fvcore/blob/master/fvcore/nn/giou_loss.py
+
+    Args:
+        input/target_offsets (Tensor): 1D offsets of size (N, 2)
+        reduction: 'none' | 'mean' | 'sum'
+                 'none': No reduction will be applied to the output.
+                 'mean': The output will be averaged.
+                 'sum': The output will be summed.
+        eps (float): small number to prevent division by zero
+    """
+    input_offsets = input_offsets.float()
+    target_offsets = target_offsets.float()
+    # check all 1D events are valid
+    assert (input_offsets >= 0.0).all(), "predicted offsets must be non-negative"
+    assert (target_offsets >= 0.0).all(), "GT offsets must be non-negative"
+
+    lp, rp = input_offsets[:, 0], input_offsets[:, 1]
+    lg, rg = target_offsets[:, 0], target_offsets[:, 1]
+
+    # intersection key points
+    lkis = torch.min(lp, lg)
+    rkis = torch.min(rp, rg)
+
+    # iou
+    intsctk = rkis + lkis
+    unionk = (lp + rp) + (lg + rg) - intsctk
+    iouk = intsctk / unionk.clamp(min=eps)
+
     # smallest enclosing box
     lc = torch.max(lp, lg)
     rc = torch.max(rp, rg)
-
-    # giou
     len_c = lc + rc
-    miouk = iouk - ((len_c - unionk) / len_c.clamp(min=eps))
 
-    loss = 1.0 - miouk
+    # offset between centers
+    rho = 0.5 * (rp - lp - rg + lg)
+
+    # giou is reduced to iou in our setting, skip unnecessary steps
+    loss = 1.0 - iouk + torch.square(rho / len_c.clamp(min=eps))
 
     if reduction == "mean":
         loss = loss.mean() if loss.numel() > 0 else 0.0 * loss.sum()
